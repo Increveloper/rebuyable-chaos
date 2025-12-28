16405a0da6b81f4d81bf496c7d8223c630616a29
Increveloper <wklaver2010@gmail.com>
2025-12-28 16:52:13 +0100

    Final bugfix attempt for today.

--- SetInterval.js
+++ SetInterval.js
@@ -6,6 +6,6 @@ setInterval(function(){
     game.rebuyables[0][0][0][0][3] = mul(game.rebuyables[0][0][0][0][2], game.rebuyables[0][0][0][0][0])
 
     for(let i = 0; i < combos.length; i++){
-        document.getElementById(combos[i][1]).textContent = combos[i][0]
+        document.getElementById(combos[i][1]).textContent = combos[i][0].toFixed(2)
     }
 }, 100)
