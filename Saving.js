08d24d5770c07b665fac1ce3c7d8e0e911a514fb
Increveloper <wklaver2010@gmail.com>
2025-12-28 16:26:37 +0100

    Added a reset method

--- Saving.js
+++ Saving.js
@@ -8,3 +8,22 @@ function SaveGame(){
 function LoadGame(){
     localStorage.getItem("game")
 }
+function HardReset(){
+    let game = {}
+    game.points = new Decimal(0)
+    game.pointgain = new Decimal(1)
+    game.rebuyables = [
+        [ //Layer
+            [ //Row
+                [ //Buyable
+                    [new Decimal(0), new Decimal(10), new Decimal(1), new Decimal(0), new Decimal(10)],
+                ],
+            ],
+            [
+                [
+                    [new Decimal(0), new Decimal(10), new Decimal(1.10), new Decimal(1), new Decimal(10)],
+                ],
+            ],
+        ],
+    ]
+}
