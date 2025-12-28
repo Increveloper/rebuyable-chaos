c934287d1f5b96520936c1ccf75c9baf76f8f673
Increveloper <wklaver2010@gmail.com>
2025-12-28 16:47:22 +0100

    Bugfix attempt #8

--- OperatorHelpers.js
+++ OperatorHelpers.js
@@ -8,7 +8,6 @@ function mul(decimalVar, amount) {
     return decimalVar.times(amount);
 }
 function div(decimalVar, amount) {
-    console.log(decimalVar, typeof decimalVar, amount, typeof amount)
     return decimalVar.div(amount);
 }
 function pow(decimalVar, amount) {
