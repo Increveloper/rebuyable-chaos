93facca5d3427e8b01a8da54f50e597de487249c
Increveloper <wklaver2010@gmail.com>
2025-12-28 16:07:30 +0100

    Bugfix attempt #2

--- CalculateBuyables.js
+++ CalculateBuyables.js
@@ -1,7 +1,7 @@
 function CalculateR1B1Base(){
-    game.rebuyables[0][0][0][2] = new Decimal(1)
+    game.rebuyables[0][0][0][0][2] = new Decimal(1)
     
-    game.rebuyables[0][0][0][2] = mul(game.rebuyables[0][0][0][2], pow(game.rebuyables[0][1][0][2], game.rebuyables[0][1][0][0]))
+    game.rebuyables[0][0][0][0][2] = mul(game.rebuyables[0][0][0][0][2], pow(game.rebuyables[0][1][0][0][2], game.rebuyables[0][1][0][0][0]))
 }
 function CalculateR2B1Base(){
 
