1edc65b0e62695b792524a0105a8689157d8f1a4
Increveloper <wklaver2010@gmail.com>
2025-12-28 16:45:36 +0100

    Bugfix attempt #7

--- CalculateResources.js
+++ CalculateResources.js
@@ -1,5 +1,5 @@
 function CalculatePoints(){
-    game.pointgain = new Decimal(0)
+    game.pointgain = new Decimal(1)
 
-    game.pointgain += game.rebuyables[0][0][0][0][3] 
+    game.pointgain = add(game.pointgain, game.rebuyables[0][0][0][0][3])
 }
