629405e2424f0f5e8cfbfe17f03fbdb3dc6bf0ed
Increveloper <wklaver2010@gmail.com>
2025-12-29 07:58:12 +0100

    Bugfix attempt #8

--- ComboVars.js
+++ ComboVars.js
@@ -1,11 +1,11 @@
 let combos = [
     [game.points, "points"], 
     [game.pointgain, "pointGain"], 
-    [game.rebuyables[0][0][0][2], "R1B1base"], 
-    [game.rebuyables[0][0][0][3], "R1B1Effect"],
-    [game.rebuyables[0][0][0][1], "R1B1cost"],
-    [game.rebuyables[0][0][0][0], "R1B1Amount"],
-    [game.rebuyables[0][1][0][2], "R2B1base"],
-    [game.rebuyables[0][1][0][3], "R2B1Effect"],
-    [game.rebuyables[0][1][0][0], "R2B1Amount"]
+    [game.rebuyables[0][0][0][0][2], "R1B1base"], 
+    [game.rebuyables[0][0][0][0][3], "R1B1Effect"],
+    [game.rebuyables[0][0][0][0][1], "R1B1cost"],
+    [game.rebuyables[0][0][0][0][0], "R1B1Amount"],
+    [game.rebuyables[0][1][0][0][2], "R2B1base"],
+    [game.rebuyables[0][1][0][0][3], "R2B1Effect"],
+    [game.rebuyables[0][1][0][0][0], "R2B1Amount"]
 ]
