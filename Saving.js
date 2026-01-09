b839c1348e71f54045d98bd4ae3db47002a6641c
Increveloper <wklaver2010@gmail.com>
2026-01-09 18:59:14 +0100

    Bugfix Attempt #10

--- Saving.js
+++ Saving.js
@@ -89,6 +89,14 @@ function HardReset(){
                     [new Decimal(0), new Decimal(10), new Decimal(1.10), new Decimal(1), new Decimal(10)],
                 ],
             ],
+            [
+                [
+                    [new Decimal(0), new Decimal(10), new Decimal(0.10), new Decimal(0), new Decimal(10)]
+                ],
+                [
+                    [new Decimal(0), new Decimal(1000), new Decimal(1.50), new Decimal(1.00), new Decimal(10)]
+                ],
+            ],
         ],
     ]
 }
